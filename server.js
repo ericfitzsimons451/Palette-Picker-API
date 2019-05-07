f2da527efe980cec3402f9f5c0364f6963681f6e
David Cisneros <Dcisneros101@gmail.com>
2019-05-07 13:45:13 -0600

    Adds GET endpoint for projects
    Co-authored-by: Justin Torrance <justin.torrance91@gmail.com>
    Co-authored-by: Eric Fitzsimons <ericfitzsimons451@gmail.com>

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.json());
 app.set('port', process.env.PORT || 3001);
 
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
 app.listen(app.get('port'), () => {
   console.log(`App is running on localhost: ${app.get('port')}.`)
 });
