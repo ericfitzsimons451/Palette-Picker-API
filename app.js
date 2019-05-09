3cfd356e1cef492c31a4308a5a932f0bd8111c56
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-08 20:40:02 -0600

    adds GET palette by ID, Delete palette by id, and POST palette endpoints

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
 app.use(express.json());
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.json());
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
 // const cors = require('cors')
 // app.use(cors())
 
@@ -35,18 +35,18 @@ app.get('/api/v1/projects/:id', (request, response) => {
     });
 });
 
-app.get('/api/v1/projects/:id/palettes', (request, response) => {
+app.get('/api/v1/projects/:id/palettes/:id', (request, response) => {
   database('palettes').where('id', request.params.id).select()
-    .then(palettes => {
-      if (palettes.length) {
-        response.status(200).json(palettes)
+    .then(palette => {
+      if (palette.length) {
+        response.status(200).json(palette)
       } else {
-        response.status(404).json({ Error: `Could not find palettes for Project_ID: ${request.params.id}`})
+        response.status(404).json({ error: `Could not find palette with id: ${request.params.id}.`})
       }
     })
-    .catch(error => {
-      response.status(500).json({ error })
-    })
+   .catch(error => {
+     response.status(500).json({ error })
+   })
 })
 
 app.post('/api/v1/projects', (request, response) => {
@@ -68,6 +68,40 @@ app.post('/api/v1/projects', (request, response) => {
     });
 });
 
+app.post('/api/v1/projects/:id/palettes', (request, response) => {
+  const palette = request.body
+  for (let requiredParam of ['project_id', 'color_one', 'color_two', 'color_three', 'color_four', 'color_five']) {
+    if (palette[requiredParam] === undefined) {
+      return response
+              .status(422)
+              .send({ error: `Expected format: {
+                project_id: <Integer>,
+                color_one: <String>,
+                color_two: <String>,
+                color_three: <String>,
+                color_four: <String>,
+                color_five: <String>
+              }.  You are missing a ${requiredParam} property.`})
+    }
+  }
+
+  let palettesToCheck;
+  database('palettes').select()
+    .then(data => palettesToCheck = data)
+    .then(() => {
+      const foundPalette = palettesToCheck.find(palette => {
+        return palette.color_one === request.body.color_one
+      })
+
+      if (!foundPalette) {
+        database('palettes').insert(palette, 'id')
+          .then(id => {
+            response.status(201).json({ id: id[0] })
+          })
+      }
+    })
+})
+
 app.delete('/api/v1/projects/:id', (request, response) => {
   database('projects').where('id', request.params.id).del()
     .then(result => {
@@ -82,6 +116,26 @@ app.delete('/api/v1/projects/:id', (request, response) => {
     .catch(error => {
       response.status(500).json(`Something went wrong with the server: ${error}`)
     })
+})
+
+app.delete('/api/v1/palettes/:id', (request, response) => {
+  const idForDelete = request.params.id
+  if (!idForDelete) {
+    response.status(422).json({
+      error: `Missing id from request parameters`
+    })
+  } else {
+    database('palettes')
+      .where('id', request.params.id)
+      .del()
+      .then(() => {
+        response
+          .json(`Successfully deleted course with id: ${idForDelete}`)
+      })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+  }
 })
 
 module.exports = app
