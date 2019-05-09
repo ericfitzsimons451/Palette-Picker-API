d17c7b446b422fc449b5b138f92509c7452b3ab1
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-09 14:12:22 -0600

    add tests for GET all palettes endpoint

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 const request = require('supertest')
-const server = require('./app')
+const app = require('./app')
 
 describe('/api/v1', () => {
 
@@ -13,11 +13,20 @@ describe('/api/v1', () => {
   describe('GET /projects', () => {
     it('should return all projects in database', async () => {
       const expected = await database('projects').select()
-      const response = await request(server).get('/api/v1/projects')
+      const response = await request(app).get('/api/v1/projects')
       expect(response.status).toBe(200)
       expect(response.body.length).toBe(expected.length)
     });
   });
 
-  
+  describe('GET /projects/:id/palettes', () => {
+    it('should return all palettes in a project based on the request.params.id', async () => {
+      const project = await database('projects').first()
+      const id = project.id
+      const expectedPalettes = await database('palettes').where('project_id', id)
+      const response = await request(app).get(`/api/v1/projects/${id}/palettes`)
+      expect(response.status).toBe(200)
+      expect(response.body.length).toBe(expectedPalettes.length)
+    })
+  })
 });
