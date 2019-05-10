72e98e5a4cdbaa0615346203fa5e2aaea2150a22
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-10 12:09:40 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'postgres://dkndpyhvhtvmmq:04ae155a91b8b10ef7f9aa3ccc10f212855ed97b2d1beedc5b888b37a48e0fac@ec2-54-235-208-103.compute-1.amazonaws.com:5432/d5t218i9skkfha',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
