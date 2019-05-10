230400848bddedbe4994d90fe77aaf6ba390ada1
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-05-10 11:55:54 -0600

    Continue Travis setup

--- knexfile.js
+++ knexfile.js
@@ -17,5 +17,16 @@ module.exports = {
       directory: './seeds'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: 'postgres://dkndpyhvhtvmmq:04ae155a91b8b10ef7f9aa3ccc10f212855ed97b2d1beedc5b888b37a48e0fac@ec2-54-235-208-103.compute-1.amazonaws.com:5432/d5t218i9skkfha',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
   }
 };
